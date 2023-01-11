2bbd97ec24bf6e733dbe2e46bfa1d8733d1f2c86
Andrew <andrewonulak@gmail.com>
2023-01-11 11:48:01 -0700

    setting up project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [fullList, setFullList] = useState([])
+
+  useEffect(() => {
+    fetch("https://www.dnd5eapi.co/api/equipment")
+    .then(r => r.json())
+    .then(d => setFullList(d))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>TESTING</h1>
+  )
 }
 
 export default App;
