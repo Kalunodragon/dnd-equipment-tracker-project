bc7f0a5f950f61a1ce9b9fa091fa68cf0fd06ffe
Andrew <andrewonulak@gmail.com>
2023-01-11 15:47:01 -0700

    working on getting the API to fetch correctly

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,23 @@ import Inventory from "./Inventory";
 import Home from "./Home";
 import Navbar from "./Navbar";
 import Form from "./Form";
+import FullEquipmentList from "./FullEquipmentList";
+import RandomTreasure from "./RandomTreasure";
 
 function App() {
   const [itemsInBag, setItemsInBag] = useState([])
+  const [itemList, setItemList] = useState([])
 
-  // const API = "https://www.dnd5eapi.co/api/equipment"
+  const API = "https://www.dnd5eapi.co/api/equipment"
 
   useEffect(() => {
     fetch("http://localhost:3001/equipment/")
     .then(r => r.json())
     .then(d => setItemsInBag(d))
+
+    fetch(API)
+    .then(r => r.json())
+    .then(d => setItemList(d))
   }, [])
 
   return (
@@ -21,14 +28,20 @@ function App() {
       <h1 id="main-header">D&D Equipment Tracker Tool</h1>
       <Navbar />
       <Switch>
-        <Route exact path="/Inventory">
-          <Inventory />
-        </Route>
         <Route exact path="/">
           <Home />
         </Route>
+        <Route exact path="/Inventory">
+          <Inventory items={itemsInBag}/>
+        </Route>
         <Route exact path="/Form">
           <Form />
+        </Route>
+        <Route exact path="/EquipmentList">
+          <FullEquipmentList items={itemList}/>
+        </Route>
+        <Route exact path="/RandomTreasure">
+          <RandomTreasure />
         </Route>
       </Switch>
     </>
--- src/components/FullEquipmentList.js
+++ src/components/FullEquipmentList.js
@@ -1,8 +1,23 @@
 import React from "react";
 
-function FullEquipmentList() {
+function FullEquipmentList({ items }) {
+    const results = items.results
+
+
+    // Find a way to add nested fetch data to show when clicking on the name of an item
+    const listed = results.map(item => {
+        return (
+            <div key={item.index}>
+                {item.name}
+            </div>
+        )
+    })
+
     return (
-        <h1>FullEquipmentList TEST</h1>
+        <div>
+            <h1>FullEquipmentList TEST</h1>
+            {listed}
+        </div>
     )
 }
 
