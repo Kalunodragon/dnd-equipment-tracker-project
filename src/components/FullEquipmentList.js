744bc36dc25a3f21fbcd8b889a5ecd2855fba1f8
Andrew <andrewonulak@gmail.com>
2023-01-11 23:48:54 -0700

    left comments in FEL for next coding session

--- src/components/FullEquipmentList.js
+++ src/components/FullEquipmentList.js
@@ -2,7 +2,8 @@ import React from "react";
 // import ItemCard from "./ItemCard";
 
 function FullEquipmentList({ items }) {
-
+// Add some kind of sorting buttons that will allow a user to filter out items looking for specific items
+// also add some sort of search feature that updated with each letter added
   return (
   	<div>
     	<h1>All 5E Items</h1>
