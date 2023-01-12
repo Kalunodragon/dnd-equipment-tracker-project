d397430364e06bec6106e039392f8844534171b8
Andrew <andrewonulak@gmail.com>
2023-01-11 22:05:37 -0700

    added basic style for ItemCard and some logic for rendering specific item information

--- src/components/FullEquipmentList.js
+++ src/components/FullEquipmentList.js
@@ -9,17 +9,20 @@ function FullEquipmentList({ items }) {
         results.forEach(item => {
             fetch(`http://www.dnd5eapi.co${item.url}`)
                 .then(r => r.json())
-                .then(d => setFetchedItems(previous => [...previous, <ItemCard item={d} />]))
-        })
+                .then(d => 
+                    setFetchedItems(previous => 
+                        [...previous, <ItemCard key={d.index} item={d} />]
+                    ))
+            })
     }, [])
-
-    // Find a way to add nested fetch data to show when clicking on the name of an item
 
 
     return (
         <div>
-            <h1>FullEquipmentList TEST</h1>
-            {fetchedItems}
+            <h1>All 5E Items</h1>
+            <div className="card-container">
+                {fetchedItems}
+            </div>
         </div>
     )
 }
--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,11 +2,15 @@ import React from "react";
 
 function ItemCard({ item }){
     console.log(item)
-    const { name, weight, equipment_category, cost, gear_category } = item
+    const { name, weight, equipment_category, cost, gear_category, speed } = item
     return(
         <div className="card">
-            <h3>{name}</h3>
+            <h2>{name}</h2>
+            <p>Type of item: {equipment_category.name}</p>
+            {gear_category ? <p>Gear type: {gear_category.name}</p> : null}
+            {speed ? <p>Speed: {speed.quantity}{speed.unit}</p> : null}
             <p>weight: {weight}</p>
+            <p>cost: {cost.quantity} {cost.unit}</p>
         </div>
     )
 }
