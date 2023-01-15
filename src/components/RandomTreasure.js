d4fe22339b07328271bb0842ef1ba68cc3c15ee4
Andrew <andrewonulak@gmail.com>
2023-01-15 00:29:06 -0700

    moved json to seperate repo, and fixed random treasure to keep only one list

--- src/components/RandomTreasure.js
+++ src/components/RandomTreasure.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function RandomTreasure({ items }) {
     const [num, setNum] = useState(0)
-    const randomList = [...items].sort(() => (Math.random() > .5 ? 1 : -1)).slice(0, num)
+    const [more, setMore] = useState(0)
+    const [display, setDisplay] = useState([])
+
+    const shown = display.slice(0, num)
+
+    useEffect(() => {
+        setDisplay([...items].sort(() => (Math.random() > .5 ? 1 : -1)))
+    },[more, items])
 
     function handleClick(e){
+        if(e.target.name ==='new'){
+            setMore(value => value + 1)
+        }
         if(e.target.name === 'more'){
             setNum(value => value + 1)
         }
@@ -17,14 +27,22 @@ function RandomTreasure({ items }) {
 
     return (
         <div className="randomTreasure">
-            <h1>RandomTreasure TEST</h1>
-            <button className="randomBtn" name="more" onClick={handleClick}>More</button>
-            <h1>{num}</h1>
-            <button className="randomBtn" name="less" onClick={handleClick}>Less</button>
+            <h1>RandomTreasure</h1>
+            <p>
+
+            </p>
+            <button className="randomBtn" name="new" onClick={handleClick}>New Random Treasure</button>
+            <br/>
+            <br/>
+            <div className="btn-container">
+                <button className="randomBtn" name="less" onClick={handleClick}>Less</button>
+                <h1>{num}</h1>
+                <button className="randomBtn" name="more" onClick={handleClick}>More</button>
+            </div>
             <br/>
             <br/>
             <div className="card-container">
-                {randomList}
+                {shown}
             </div>
         </div>
     )
